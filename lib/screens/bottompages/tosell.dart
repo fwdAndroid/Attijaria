0663fd0ee6400429d55fb38d1912607648cb3f67
fwdAndroid <fwdkaleem@gmail.com>
2021-11-06 14:26:33 +0500

    Fawad

--- lib/screens/bottompages/tosell.dart
+++ lib/screens/bottompages/tosell.dart
@@ -10,6 +10,38 @@ class ToSell extends StatefulWidget {
 class _ToSellState extends State<ToSell> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(children: [
+        Container(
+          height: 80,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.black,
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('asset/profilepic.png'),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'Fawad Kaleem',
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+        Container(
+          child: Row(
+            children: [TextFormField()],
+          ),
+        )
+      ]),
+    );
   }
 }
