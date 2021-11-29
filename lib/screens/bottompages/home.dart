012b7dfc2c9909197292fbc3629e47b39abe3743
fwdAndroid <fwdkaleem@gmail.com>
2021-11-29 09:56:58 +0500

    Add

--- lib/screens/bottompages/home.dart
+++ lib/screens/bottompages/home.dart
@@ -43,6 +43,7 @@ class _HomeState extends State<Home> {
           ),
         ),
         body: ListView(
+          controller: _scrollController,
           children: [
             GestureDetector(
               onTap: () {
