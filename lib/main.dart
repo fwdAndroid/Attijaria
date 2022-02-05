06c64b5b830d736b9911a42878aeed48329cb7c5
fwdAndroid <fwdkaleem@gmail.com>
2022-02-05 14:58:33 +0500

    Fawad

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:attijaria/screens/Home/homepage.dart';
-import 'package:attijaria/screens/bottompages/home.dart';
 import 'package:attijaria/screens/introscreens/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: HomePage(),
+      home: Welcome(),
     );
   }
 }
