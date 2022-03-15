de34f47edb04a931c06c5cfc17f21336a3788fdc
fwdAndroid <fwdkaleem@gmail.com>
2022-03-15 08:33:59 +0500

    Fawad

--- lib/separaterpages/categoriesselector.dart
+++ lib/separaterpages/categoriesselector.dart
@@ -1,4 +1,5 @@
 import 'package:attijaria/Providers/allproviders.dart';
+import 'package:attijaria/separaterpages/adsNew.dart';
 import 'package:attijaria/separaterpages/selectsubcategaries.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -45,25 +46,25 @@ class _CategoriesSelectorState extends State<CategoriesSelector> {
                             },
                             child: Row(
                               children: [
-                                Icon(Icons.laptop),
+                                Image.asset('assets/gg.png',height: 30,width: 30,),
                                 SizedBox(width: 20),
                                 Text(
                                   'Computing and Multimedia',
-                                  style: TextStyle(fontSize: 12),
+                                  style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
                                 )
                               ],
                             ),
                           ),
                           Divider(),
-                          // Text('Telephones'),
-                          // Text('Tablet'),
-                          // Text('Computers portables'),
-                          // Text('Computers Accessorie'),
-                          // Text('Video Game Console'),
-                          // Text('Cameras and Cameras'),
-                          // Text('Television'),
-                          // Text('Image & Sons'),
-                          // Divider(),
+                          Text('Telephones'),
+                          Text('Tablet'),
+                          Text('Computers portables'),
+                          Text('Computers Accessorie'),
+                          Text('Video Game Console'),
+                          Text('Cameras and Cameras'),
+                          Text('Television'),
+                          Text('Image & Sons'),
+                          Divider(),
                           InkWell(
                             onTap: (){
                               Provider.of<AllpProviders>(context,listen: false).setCetagory("Vehicles");
@@ -72,26 +73,26 @@ class _CategoriesSelectorState extends State<CategoriesSelector> {
 
                             child: Row(
                               children: [
-                                Icon(Icons.car_rental),
+                                 Image.asset('assets/vw.png',height: 30,width: 30,),
                                 SizedBox(width: 20),
                                 Text(
                                   'Vehicles',
-                                  style: TextStyle(fontSize: 12),
+                                  style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
                           ),
-                          // Divider(),
-                          // Text('Cars'),
-                          // Text('Car rental'),
-                          // Text('Parts and Accessories'),
-                          // Text('Motors'),
-                          // Text('Velos'),
-                          // Text('Arrow'),
-                          // Text('Engins BTP'),
-                          // Text('Engine Agircole'),
-                          // Text('Trucks'),
-                          // Text('Others'),
+                          Divider(),
+                          Text('Cars'),
+                          Text('Car rental'),
+                          Text('Parts and Accessories'),
+                          Text('Motors'),
+                          Text('Velos'),
+                          Text('Arrow'),
+                          Text('Engins BTP'),
+                          Text('Engine Agircole'),
+                          Text('Trucks'),
+                          Text('Others'),
                           Divider(),
                           InkWell(
                             onTap: (){
@@ -100,23 +101,23 @@ class _CategoriesSelectorState extends State<CategoriesSelector> {
                             },
                             child: Row(
                               children: [
-                                Icon(Icons.home),
+                                 Image.asset('assets/hw.png',height: 30,width: 30,),
                                 SizedBox(width: 20),
                                 Text(
                                   'Real Estate',
-                                  style: TextStyle(fontSize: 12),
+                                  style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
                                 )
                               ],
                             ),
                           ),
-                          // Divider(),
-                          // Text('Appartments'),
-                          // Text('Maisons and Villas'),
-                          // Text('Desks and Trays'),
-                          // Text('Magsins'),
-                          // Text('Lands and Farms'),
-                          // Text('Other Real Estate'),
-                          //
+                          Divider(),
+                          Text('Appartments'),
+                          Text('Maisons and Villas'),
+                          Text('Desks and Trays'),
+                          Text('Magsins'),
+                          Text('Lands and Farms'),
+                          Text('Other Real Estate'),
+                          
                           Divider(),
                            InkWell(
                              onTap: (){
@@ -125,20 +126,20 @@ class _CategoriesSelectorState extends State<CategoriesSelector> {
                                },
                              child: Row(
                               children: [
-                                Icon(Icons.gradient),
+                                Image.asset('assets/v.png',height: 30,width: 30,),
                                 SizedBox(width: 20),
                                 Text(
                                   'FOR HOME AND GARDEN',
-                                  style: TextStyle(fontSize: 12),
+                                  style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),
                                 )
                               ],
                           ),
                            ),
-                          // Divider(),
-                          // Text('Appliances and Tableware'),
-                          // Text('Furniture and Decoration'),
-                          // Text('Garden and DIY Tools')
-                          //
+                          Divider(),
+                          Text('Appliances and Tableware'),
+                          Text('Furniture and Decoration'),
+                          Text('Garden and DIY Tools')
+                          
                         ],
                       ),
                     ),
@@ -168,7 +169,7 @@ class _CategoriesSelectorState extends State<CategoriesSelector> {
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
             child: ElevatedButton(onPressed: (){
-         if(Provider.of<AllpProviders>(context,listen: false).cetagory!.isEmpty){}else if(Provider.of<AllpProviders>(context,listen: false).cetagory!.isNotEmpty){     Navigator.push(context, MaterialPageRoute(builder: (builder) => SelectSubCategories()));
+         if(Provider.of<AllpProviders>(context,listen: false).cetagory!.isEmpty){}else if(Provider.of<AllpProviders>(context,listen: false).cetagory!.isNotEmpty){     Navigator.push(context, MaterialPageRoute(builder: (builder) => AddNewsPosts(cetagory: 'jh',)));
          }
             }, child: Text('Continue'),style: ElevatedButton.styleFrom(fixedSize: Size(300, 50),primary: Color(0xffF8B800)),))
         ],
